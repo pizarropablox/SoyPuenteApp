fd24f711f6938072420abf436c672f06537fa9f2
pizarropablox <32684457+pizarropablox@users.noreply.github.com>
2024-10-03 23:56:55 -0300

    a

--- app/src/main/java/com/duoc/soypuenteapp/pages/SignupPage.kt
+++ app/src/main/java/com/duoc/soypuenteapp/pages/SignupPage.kt
@@ -24,14 +24,37 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.duoc.soypuenteapp.AuthViewModel
 import com.duoc.soypuenteapp.R
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import androidx.compose.material3.*
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import com.duoc.soypuenteapp.Usuario
+
 
 @Composable
 fun SignupPage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel){
+
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
 
     var email by remember {
         mutableStateOf("")
     }
     var password by remember {
+        mutableStateOf("")
+    }
+    var confirmPassword by remember {
         mutableStateOf("")
     }
     var nombre by remember {
@@ -40,6 +63,7 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
     var direccion by remember {
         mutableStateOf("")
     }
+
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -62,6 +86,7 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
                 Text(text = "Correo")
             }
         )
+        //CONTRASEÑA
         OutlinedTextField(
             value = password,
             onValueChange = {
@@ -69,9 +94,22 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
             },
             label = {
                 Text(text = "Contraseña")
-            }
+            },
+            textStyle = TextStyle(fontSize = 24.sp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        // CONFIRMAR CONTRASEÑA
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = {
+                Text(text = "Confirmar Contraseña")
+            },
+            textStyle = TextStyle(fontSize = 24.sp),
+            visualTransformation = PasswordVisualTransformation()
         )
 
+        //NOMBRE
         OutlinedTextField(
             value = nombre,
             onValueChange = {
@@ -82,6 +120,7 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
             }
         )
 
+        // DIRECCION
         OutlinedTextField(
             value = direccion,
             onValueChange = {
@@ -95,7 +134,39 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = {
+            if (password == confirmPassword) {
+                // Crear usuario con Firebase Authentication
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Obtener el usuario recién registrado
+                            val user = auth.currentUser
 
+                            // Guardar los datos del usuario en Firestore
+                            val userData = hashMapOf(
+                                "uid" to user?.uid,
+                                "email" to user?.email,
+                                "provider" to "Email/Password",
+                                "creationDate" to System.currentTimeMillis().toString()
+
+                            )
+
+                            db.collection("usuarios").document(user?.uid!!)
+                                .set(userData)
+                                .addOnSuccessListener {
+                                    Toast.makeText(context, "Usuario registrado y guardado", Toast.LENGTH_SHORT).show()
+                                    navController.navigate("login") // Navegar al login
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(context, "Error al guardar en Firestore", Toast.LENGTH_SHORT).show()
+                                }
+                        } else {
+                            Toast.makeText(context, "Error al registrar en Firebase Auth", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(context, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
         }) {
             Text(text = "REGISTRARME")
         }
